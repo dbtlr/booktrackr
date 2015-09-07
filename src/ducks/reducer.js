13e89dc01c2589d3d6f05ac232466e3081f3c9f2
Drew Butler <hi@dbtlr.com>
2015-09-07 18:47:22 -0400

    Clean up the old reducers.

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,17 +1,9 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth';
-import counter from './counter';
-import {reducer as form} from 'redux-form';
-import info from './info';
-import widgets from './widgets';
 import books from './books';
 
 export default combineReducers({
   auth,
-  counter,
-  form,
-  info,
-  widgets,
   books
 });
